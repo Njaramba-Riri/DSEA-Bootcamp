5d2d9650bc39df68c94212c62485ac44b3dded21
Riri Njaramba <jamesriri03@gmail.com>
2023-02-22 19:35:22 +0300

    Update

---
+++ Hackerank/hacker.py
@@ -0,0 +1,15 @@
+def FuzzBuzz(n):
+    for i in range(n, n+1):
+        if i%3 and i%5==0:
+            print("FizzBuzz")
+        elif i%3==0:
+            print("Fizz")
+        elif i%5==0:
+            print("Buzz")
+        else:
+            print(i)
+
+if __name__=='__main__':
+    n=int(input().strip())
+
+    FuzzBuzz(n)
--- ccm.py
+++
@@ -1,9 +0,0 @@
-def fizzbuzz(n):
-    for i in range(1,16):
-
-if __name__=='__main__':
-    print("Fizz")
-elif __name__!='__main__':
-    print("Buzz")
-else:
-    print(n)
