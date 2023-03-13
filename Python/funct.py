b5dc2bba1c7f41f624c6639597cfbf3bdba67756
Riri Njaramba <jamesriri03@gmail.com>
2023-03-13 23:29:37 +0300

    update

--- Python/funct.py
+++ Python/funct.py
@@ -14,5 +14,19 @@ x("world")
 
 
 list(map(int, ['1','2','3']))
+
 function_list=[h,x]
 function_list
+
+
+
+#python functonality purity
+def native_sum(list):
+    s=0
+
+    for i in list:
+      s +=1
+    return s
+
+
+sum(list)#replaces the above code
